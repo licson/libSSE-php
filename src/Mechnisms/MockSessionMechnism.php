cb0da867351f1872e7ea1b8729a1aae42225f720
Tony Yip <tony@opensource.hk>
2016-04-28 01:06:50 +0800

    Add SessionLike Mechnism

---
+++ src/Mechnisms/MockSessionMechnism.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * libSSE-php
+ *
+ * Copyright (C) Tony Yip 2016.
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"),
+ * to deal in the Software without restriction,
+ * including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS",
+ * WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @category libSSE-php
+ * @author   Tony Yip <tony@opensource.hk>
+ * @license  http://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Sse\Mechnisms;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class MockSessionMechnism extends AbstractMechnism
+{
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(array $param)
+    {
+        parent::__construct($param);
+        if (!isset($param['interface']) || ! $param['interface'] instanceof SessionInterface) {
+            throw new \InvalidArgumentException('SessionInterface is missed');
+        }
+
+        $this->session = $param['interface'];
+    }
+
+    public function get($key)
+    {
+        if (!$this->session->isStarted())
+            $this->session->start();
+
+        $value =  $this->session->get($key);
+        $this->session->save();
+        return $value;
+    }
+
+    public function set($key, $value)
+    {
+        try {
+            if (!$this->session->isStarted()) {
+                $this->session->start();
+            }
+
+            $this->session->set($key, $value);
+            $this->session->save();
+
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function delete($key)
+    {
+        if (!$this->session->isStarted()) {
+            $this->session->start();
+        }
+
+        $this->session->remove($key);
+        $this->session->save();
+    }
+
+    public function has($key)
+    {
+        if (!$this->session->isStarted()) {
+            $this->session->start();
+        }
+
+        $exists = $this->session->has($key);
+        $this->session->save();
+        return $exists;
+    }
+}
