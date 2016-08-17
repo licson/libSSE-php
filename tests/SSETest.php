ee9c02168943295ac2c6cfe6e659ef731308107f
Tony Yip <tony@opensource.hk>
2016-08-18 03:54:55 +0800

    Update package and add more test

--- tests/SSETest.php
+++ tests/SSETest.php
@@ -53,7 +53,7 @@ class SSETest extends \PHPUnit_Framework_TestCase
         $request = Request::create('/', 'GET', array(), array(), array(), array('HTTP_LAST_EVENT_ID' => 5));
         $sse = new SSE($request);
         $this->assertAttributeEquals(5, 'id', $sse);
-        $this->assertEquals(true, $sse->is_reconnect);
+        $this->assertTrue($sse->is_reconnect);
     }
 
     public function testCreateResponse()
@@ -86,6 +86,32 @@ class SSETest extends \PHPUnit_Framework_TestCase
     {
         $sse = new SSE();
         $sse->start();
+    }
+
+    public function testHasEventListener()
+    {
+        $sse = new SSE();
+        $this->assertFalse($sse->hasEventListener());
+    }
+
+    public function testGetEventListener()
+    {
+        $sse = new SSE();
+        $this->assertEquals(array(), $sse->getEventListeners());
+    }
+
+    public function testSetStart()
+    {
+        $sse = new SSE();
+        $sse->setStart(123456);
+        $this->assertAttributeEquals(123456, 'start', $sse);
+    }
+
+    public function testGetStart()
+    {
+        $sse = new SSE();
+        $sse->setStart(123456);
+        $this->assertEquals(123456, $sse->getStart());
     }
 }
 
